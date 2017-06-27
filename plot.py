0230a69779687824808136706281dea53f71b514
Aleksandr Mykhailets <a.mikhaylets@Aleksandrs-MacBook-Pro.local>
2017-06-27 22:47:15 +0200

    added ggplot chart

--- plot.py
+++ plot.py
@@ -1,7 +1,15 @@
+#Practicing in plotting with matplotlib
+#Import pyplot from matpotlib package
+import numpy as np
+import ggplot as gp
 from matplotlib import pyplot as plt
 
-deviation = [1, 2, 4, 7, 7, 4, 3, 1, 4, 6, 8, 11]
-deviations = [1, 2, 4, 7, 7, 4, 3, 1, 4, 6, 8, 11]
+x = np.arange(0, 1, 0.05)
+y = np.power(x, 2)
 
-plt.scatter(deviation, deviations)
+plt.scatter(x, y)
+plt.grid(b='on')
 plt.show()
+
+gp.qplot(x, y)
+help(plt.grid)
