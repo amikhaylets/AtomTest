1efcfd99f86c3cd1bb4219058b06b8643e56a99c
Aleksandr Mykhailets <a.mikhaylets@Aleksandrs-MacBook-Pro.local>
2017-06-27 19:32:16 +0200

    plot changed to scatter

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 from matplotlib import pyplot as plt
 
 deviation = [1, 2, 4, 7, 7, 4, 3, 1, 4, 6, 8, 11]
+deviations = [1, 2, 4, 7, 7, 4, 3, 1, 4, 6, 8, 11]
 
-plt.plot(deviation)
+plt.scatter(deviation, deviations)
 plt.show()
