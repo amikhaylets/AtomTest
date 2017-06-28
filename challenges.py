84183dec58a2f481591e13d05adb7f0f0cccc5f1
Aleksandr Mikhaylets <a.mikhaylets@yahoo.com>
2017-06-28 23:07:42 +0200

    Initial commit

---
+++ challenges.py
@@ -0,0 +1,10 @@
+#daily datacamp challenges
+
+p = ['q', 'k', 'u', 'w']
+q = p + ['d']
+print(q)
+
+# Find the standard deviation of x
+import numpy as np
+x = np.array([0.87, 0.29, 2.01])
+print(np.std(x))
